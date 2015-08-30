b693365ce150e4af07ee11a454be829608b7733d
molegom <olego@i.ua>
2015-08-31 00:25:39 +0300

    Add js file

---
+++ BestFiendsWeb/JS/Field.js
@@ -0,0 +1,9 @@
+﻿$(window).load(function () {
+    $("td").dblclick(function () {
+        $(this).css("background-color", "#00FF00");
+
+    });
+    $("td").click(function () {
+        $(this).css("background-color", "#FF0000");
+    });
+});
