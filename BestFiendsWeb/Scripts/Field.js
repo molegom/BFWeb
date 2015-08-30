8f06dc7b46c7faa8873393cf98bd87fb135c30e9
molego <oleg.murashko@preciseq.com>
2015-08-30 22:19:48 +0300

    A2

--- BestFiendsWeb/Scripts/Field.js
+++ BestFiendsWeb/Scripts/Field.js
@@ -1,5 +1,10 @@
 ﻿$(window).load(function () {
     $("td").mouseleave(function () {
         //$(this).css("background-color", "#FF0000");
+
+    });
+    $("td").click(function () {
+        //$(this).css("background-color", "#FF0000");
+
     });
 });
